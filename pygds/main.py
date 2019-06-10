324951cf510ff2246ccab500fa914f189e0e6b1d
Mbaye Hann <mbaye@ctsfares.com>
2019-06-10 12:15:43 -0400

    main gds

--- pygds/main.py
+++ pygds/main.py
@@ -2,7 +2,7 @@ from pygds.sabre.reservation import SabreReservation
 
 class GDS ():
 
-    def get_reservation(self,gds, pnr, pcc, conversation_id):
+    def get_reservation(self,gds=None, pnr=None, pcc=None, conversation_id=None):
 
         if gds.upper() == 'SABRE':
             return SabreReservation().get(pnr, pcc, conversation_id)
