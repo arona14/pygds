e1a70aa3eedb4a0da4687009ab2e71a7f3489a0d
Ibrahima Ndiaye <eliusxpol@hotmail.com>
2019-06-10 14:41:31 +0000

    Comment to the testmain

--- pygds/tests/test_main.py
+++ pygds/tests/test_main.py
@@ -3,6 +3,8 @@ from unittest import TestCase
 from pygds.main import get_reservation
 
 class TestMain(TestCase):
+    # Just wanted to make sure that the function is accessible
+    # These tests will be changed to real tests
     def test_get_reservation_ok(self):
         result = get_reservation('sabre', 'abc123', 'wr17', 'test')
         self.assertTrue(isinstance(result, str))
