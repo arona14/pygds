906a3d5d0c883573c158eee4f2929b1723f612db
Mbaye Hann <mbaye@ctsfares.com>
2019-06-10 12:15:06 -0400

    cdc

--- pygds/main.py
+++ pygds/main.py
@@ -2,7 +2,7 @@ from pygds.sabre.reservation import SabreReservation
 
 class GDS ():
 
-    def get_reservation(gds, pnr, pcc, conversation_id):
+    def get_reservation(self,gds, pnr, pcc, conversation_id):
 
         if gds.upper() == 'SABRE':
             return SabreReservation().get(pnr, pcc, conversation_id)
