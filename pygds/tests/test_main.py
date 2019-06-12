67363ac775c34dd6a9a6408f56df17485e22088a
Mbaye Hann <mbaye@ctsfares.com>
2019-06-12 12:36:03 -0400

    test main test_get_reservation_ok

--- pygds/tests/test_main.py
+++ pygds/tests/test_main.py
@@ -6,10 +6,10 @@ class TestMain(TestCase):
     # Just wanted to make sure that the function is accessible
     # These tests will be changed to real tests
     def test_get_reservation_ok(self):
-        result = GDS.get_reservation('sabre', 'abc123', 'wr17', 'test')
-        self.assertTrue(isinstance(result, str))
+        result = GDS.get_reservation('sabre', 'JUBGEV', 'WR17', 'test')
+        self.assertTrue(isinstance(result, object))
 
     def test_send_command_ok(self):
        
         result = GDS().send_command('sabre', '*JUBGEV','WR17', conversation_id='cosmo-material-b6851be0-b83e-11e8-be20-c56b920f05b5')
-        self.assertTrue(isinstance(result,str))
+        self.assertTrue(isinstance(result,object))
