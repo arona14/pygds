77032b3100c766d8118e951aee4aa903915e8bea
Mbaye Hann <mbaye@ctsfares.com>
2019-06-13 10:40:29 -0400

    update SabreCommend send

--- pygds/sabre/command.py
+++ pygds/sabre/command.py
@@ -1,6 +1,6 @@
 from .xmlbuilders.builder import SabreXMLBuilder
 from .session import SabreSession
-import requests
+import requests,json,xmltodict
 
 class SabreCommand :
     """ this class contains all sabre commande """
@@ -19,6 +19,10 @@ class SabreCommand :
         command_xml = SabreXMLBuilder().sabreCommandLLSRQ(pcc,token,conversation_id,command)
         command_xml = command_xml.encode('utf-8')
         response = requests.post(self.url, data=command_xml, headers=self.headers)
+        send_command = json.loads(json.dumps(xmltodict.parse(response.content)))
+        send_command = str(send_command).replace("@","")
+        send_command = send_command.replace("u'","'") 
+        send_command = eval(send_command)
         if need_close :
-            SabreSession().close(pcc,token,conversation_id)
-        return response.content 
+            SabreSession().close(pcc, token, conversation_id)
+        return send_command 
