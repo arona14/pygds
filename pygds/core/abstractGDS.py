581539741caf3da920a441c7b82d0fba3838bb55
Mouhamad ND. THIAM <mohamed@ctsfares.com>
2019-06-24 17:27:52 +0000

    move methonds to AbstractGDS

--- pygds/core/abstractGDS.py
+++ pygds/core/abstractGDS.py
@@ -6,10 +6,40 @@ from abc import ABC, abstractmethod
 
 class AbstractGDS(ABC):
 
-    def __init__(self, name):
-        self.gds_name = name
+    def __init__(self, code, name=None):
+        self.gds_code = code
+        self.gds_name = code
         super(AbstractGDS, self).__init__()
 
-    @abstractmethod
-    def get_reservation(self, pnr):
-        pass
+    def search_flights(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def get_reservation(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def create_reservation(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def price(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def ticket(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def void(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def cancel(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def exchange_segment(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def refund(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def rebook_segment(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
+
+    def update_passenger(self, parameter_list):
+        raise NotImplementedError("This method is not yet implemented in the specific GDS")
