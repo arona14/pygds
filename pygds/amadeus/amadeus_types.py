f6c800b3aa5323654531eb705a021b88be66cec5
Mouhamad THIAM <mohamed@ctsfares.com>
2019-07-01 19:09:57 +0000

    Remove status from AmadeusSessionInfo

--- pygds/amadeus/amadeus_types.py
+++ pygds/amadeus/amadeus_types.py
@@ -2,11 +2,10 @@ class AmadeusSessionInfo():
     """
     This class is for containing information for a session
     """
-    def __init__(self, security_token, sequence_number, session_id, status_code):
+    def __init__(self, security_token, sequence_number, session_id):
         self.security_token = security_token
         self.sequence_number = sequence_number
         self.session_id = session_id
-        self.status_code = status_code
 
     def is_ok(self):
         return self.status_code == 'P'
