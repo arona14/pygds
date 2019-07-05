dd3c0c3f611efbb649b42a6749e5d51e2413c96f
Demba <demba@ctsfares.com>
2019-07-05 10:52:30 +0000

    Ticketing wip

--- pygds/amadeus/ticket.py
+++ pygds/amadeus/ticket.py
@@ -11,7 +11,7 @@ class AmadeusTicketing:
 
     def add_form_of_payment(self, message_id, session_id, sequence_number, security_token, form_of_payment, passenger_reference_type, passenger_reference_value, form_of_payment_sequence_number, form_of_payment_code, group_usage_attribute_type, company_code, form_of_payment_type, vendor_code, carte_number, security_id, expiry_date):
         try:
-            token_session = "M5JDSKVU1HGPM4PAXTTOCPKJ"
+            token_session = "2I2R99GX9MUSP7UNGQ7EIZYJ6"
             url = "https://nodeD1.test.webservices.amadeus.com"
             header = {'Content-Type': 'text/xml;charset=UTF-8', 'Accept-Encoding': 'gzip,deflate', 'SOAPAction': 'http://webservices.amadeus.com/TFOPCQ_15_4_1A'}
             endpoint = get_setting("AMADEUS_ENDPOINT_URL")
@@ -21,7 +21,7 @@ class AmadeusTicketing:
             wsap = get_setting("AMADEUS_WSAP")
             add_fop = AmadeusXMLBuilder(endpoint, username, password, office_id, wsap).add_form_of_payment(message_id, session_id, sequence_number, security_token, form_of_payment, passenger_reference_type, passenger_reference_value, form_of_payment_sequence_number, form_of_payment_code, group_usage_attribute_type, company_code, form_of_payment_type, vendor_code, carte_number, security_id, expiry_date)
             response = requests.post(url, data=add_fop, headers=header)
-            #print(response.content)
+            print(response.content)
             status_code = response.status_code
         except Exception as e:
             print(e)
@@ -31,7 +31,7 @@ class AmadeusTicketing:
 
     def tiketing(self, message_id, session_id, sequence_number, security_token, passenger_reference_type, passenger_reference_value):
         try:
-            token_session = "1T488CGPNHEHQLNBSSB4FD4XA"
+            token_session = "2I2R99GX9MUSP7UNGQ7EIZYJ6"
             url = "https://nodeD1.test.webservices.amadeus.com"
             header = {'Content-Type': 'text/xml;charset=UTF-8', 'Accept-Encoding': 'gzip,deflate', 'SOAPAction': 'http://webservices.amadeus.com/TTKTIQ_15_1_1A'}
             endpoint = get_setting("AMADEUS_ENDPOINT_URL")
@@ -50,9 +50,9 @@ class AmadeusTicketing:
 
 # df test():
 message_id = "S32B6N"
-session_id = "01B2YE0PVN"
+session_id = "001M3SSTV1"
 pnr = "S32B6N"
-security_token = "1T488CGPNHEHQLNBSSB4FD4XA"
+security_token = "2I2R99GX9MUSP7UNGQ7EIZYJ6"
 compagniId = "1A"
 agenceId = "FMSU"
 vendor_code = "CA"
@@ -61,7 +61,7 @@ security_id = "737"
 expiry_date = "1020"
 form_of_payment = "FP"
 passenger_reference_type = "PT"
-passenger_reference_value = "2"
+passenger_reference_value = "3"
 form_of_payment_sequence_number = "1"
 form_of_payment_code = "CCVI"
 group_usage_attribute_type = "FP"
@@ -70,7 +70,7 @@ form_of_payment_type = "CC"
 sequence_number = "1"
 
 
-print(AmadeusTicketing().add_form_of_payment(message_id, session_id, sequence_number, security_token, form_of_payment, passenger_reference_type, passenger_reference_value, form_of_payment_sequence_number, form_of_payment_code, group_usage_attribute_type, company_code, form_of_payment_type, vendor_code, carte_number, security_id, expiry_date))
+#print(AmadeusTicketing().add_form_of_payment(message_id, session_id, sequence_number, security_token, form_of_payment, passenger_reference_type, passenger_reference_value, form_of_payment_sequence_number, form_of_payment_code, group_usage_attribute_type, company_code, form_of_payment_type, vendor_code, carte_number, security_id, expiry_date))
 
 print(AmadeusTicketing().tiketing(message_id, session_id, sequence_number, security_token, passenger_reference_type, passenger_reference_value))
 # if __name__ == "__main__":
