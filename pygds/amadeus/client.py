abb0dc66c6a579e1aea6ffd829a955afe18cae63
Mouhamad THIAM <mohamed@ctsfares.com>
2019-07-08 16:32:41 +0000

    add a session holder to the Amadeus client

--- pygds/amadeus/client.py
+++ pygds/amadeus/client.py
@@ -13,6 +13,7 @@ import requests
 from .response_extractor import PriceSearchExtractor, ErrorExtractor, SessionExtractor
 from .errors import ClientError, ServerError
 from .xmlbuilders.builder import AmadeusXMLBuilder
+from .sessions import SessionHolder
 
 
 class AmadeusClient:
@@ -25,6 +26,7 @@ class AmadeusClient:
         self.password = password
         self.office_id = office_id
         self.xmlbuilder: AmadeusXMLBuilder = AmadeusXMLBuilder(endpoint, username, password, office_id, wsap)
+        self.session_holder = SessionHolder()
         self.header_template = {'Content-Type': 'text/xml;charset=UTF-8', 'Accept-Encoding': 'gzip,deflate'}
 
     def __request_wrapper(self, method_name, request_data, soap_action):
@@ -50,7 +52,7 @@ class AmadeusClient:
 
     def start_new_session(self):
         """
-            This method starts a new session to Amadeus
+            This method starts a new session to Amadeus.
         """
         request_data = self.xmlbuilder.start_transaction(None, self.office_id, self.username, self.password, None, None)
         response_data = self.__request_wrapper("start_new_session", request_data, 'http://webservices.amadeus.com/VLSSOQ_04_1_1A')
@@ -58,7 +60,7 @@ class AmadeusClient:
 
     def fare_master_pricer_travel_board_search(self, origin, destination, departure_date, arrival_date):
         """
-            A method for searching prices of an itinerary
+            A method for searching prices of an itinerary.
         """
         request_data = self.xmlbuilder.fare_master_pricer_travel_board_search(self.office_id, origin, destination, departure_date, arrival_date)
         response_data = self.__request_wrapper("fare_master_pricer_travel_board_search", request_data, 'http://webservices.amadeus.com/FMPTBQ_18_1_1A')
@@ -68,7 +70,7 @@ class AmadeusClient:
     def fare_price_pnr_with_booking_class(self, message_id, session_id, sequence_number, security_token):
         """
             Price a PNR with a booking class.
-            The PNR is supposed to be supplied in the session on a previous call
+            The PNR is supposed to be supplied in the session on a previous call.
         """
         request_data = self.xmlbuilder.fare_price_pnr_with_booking_class(message_id, session_id, sequence_number, security_token)
         response_data = self.__request_wrapper("fare_price_pnr_with_booking_class", request_data, 'http://webservices.amadeus.com/TPCBRQ_18_1_1A')
