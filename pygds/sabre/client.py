41113c733239c6950a72672343d95ab10030ef40
Virginiesy <virginie@ctsfares.com>
2019-08-06 09:31:44 +0000

    add sabre display pnr for pygds

--- pygds/sabre/client.py
+++ pygds/sabre/client.py
@@ -1,13 +1,16 @@
-import requests
+# This file will be change for refactoring purpose.
+# This file is for Sabre reservation classes and functions
+# TODO: Use "import" statements for packages and modules only, not for individual classes or functions.
+# Note that there is an explicit exemption for
 
+import requests,jxmlease
 from pygds.core.client import BaseClient
-from pygds.sabre.session import SabreSession
-from pygds.sabre.xmlbuilders.builder import SabreXMLBuilder
-from pygds.sabre.helpers import soap_service_to_json
-from pygds.sabre.jsonbuilders.builder import  SabreBFMBuilder
-
 from pygds.core.request import  LowFareSearchRequest
-
+from pygds.core.sessions import SessionInfo
+from pygds.sabre.xmlbuilders.builder import SabreXMLBuilder
+from pygds.sabre.helpers import get_data_from_json as from_json
+from pygds.sabre.jsonbuilders.builder import  SabreBFMBuilder
+from pygds.core.app_error import ApplicationError
 
 
 class SabreClient(BaseClient):
@@ -24,48 +27,71 @@ class SabreClient(BaseClient):
         This will open a new session
         :return: a token session
         """
+        message_id = "Virginie"
         open_session_xml = self.xml_builder.session_create_rq()
         response = self._request_wrapper(open_session_xml, None)
-        return response.content
+        r = jxmlease.parse(response.content)
+        token = r[u'soap-env:Envelope'][u'soap-env:Header'][u'wsse:Security'][u'wsse:BinarySecurityToken']
+        session_info = SessionInfo(token, 1, token, message_id, False)
+        self.add_session(session_info)
+        return session_info
 
-        # r = jxmlease.parse(response.content)
-        # token = r[u'soap-env:Envelope'][u'soap-env:Header'][u'wsse:Security'][u'wsse:BinarySecurityToken']
-
-    def close_session(self, pcc, conversation_id, token_session):
+    def close_session(self,token_session):
         """
         A method to close a session
-        :param pcc: The PCC
-        :param conversation_id: the conversation id
         :param token_session: the token session
         :return: None
         """
-        SabreSession().close(pcc, conversation_id, token_session)
+        return self.xml_builder.session_close_rq(token_session)
 
-    def get_reservation(self, pnr: str, pcc: str, conversation_id: str, need_close=True):
+    def get_reservation(self,pnr:str, message_id:str ,need_close = True):
         """
         retrieve PNR
         :param pnr: the record locator
-        :param pcc: the PCC
-        :param conversation_id: The conversation id
+        :param message_id: the message identifier
         :param need_close: close or not the session
         :return: a Reservation object
         """
-        try:
-            token_session = self.open_session(pcc, conversation_id)
+        _, _, token = self.get_or_create_session_details(message_id)
+        session_info = None
+        if not token:
+            session_info = self.open_session()
+            token = session_info.security_token
 
-            get_reservation = SabreXMLBuilder().get_reservation_rq(pcc, conversation_id, token_session, pnr)
-            response = requests.post(self.url, data=get_reservation, headers=self.headers)
+        get_reservation = self.xml_builder.get_reservation_rq(token, pnr)
+        response = requests.post(self.xml_builder.url, data=get_reservation, headers=self.header_template)
 
-            to_return = soap_service_to_json(response.content)
-            to_return_dict = to_return
+        to_return = from_json(response.content,"stl18:GetReservationRS")
+  
+        if need_close:
+            self.close_session(token)
 
-            if need_close:
-                self.close_session(pcc, conversation_id, token_session)
-        except:
-            # TODO: Capture the real exception not the general one
-            to_return_dict = None
-        return to_return_dict
-
+        return to_return
+        
+    def search_price_quote(self, token_session, retain, commission, tour_code, fare_type, ticket_designator, segment_number, name_select, passenger_type, plus_up):
+        """
+        A method to cancel segment
+        :param token_session: the token session
+        :return: None
+        """
+        return self.xml_builder.price_quote_rq(token_session,retain,commission,tour_code,fare_type,ticket_designator,segment_number,name_select,passenger_type,plus_up)
+    
+    def cancel_list_segment(self,token_session,list_segment):
+        """
+        A method to cancel segment
+        :param token_session: the token session
+        :param retain: the token session
+        :param commission: the commission is used to specify the numeric amount or  the precentage of commission being claimed if applicable
+        :param tour_code: the token session
+        :param fare_type: the token session
+        :param ticket_designator: the token session
+        :param segment_number: the segment number is used to instruct the system to price specified itinerary segments
+        :param name_select: the name select is used to instruct the system to price theitinerary based upon a particular name field
+        :param passenger_type: the passenger type is used to specify a passenger type code.
+        :param plus_up: the plus up is used to specify an amount to add on top of the fare
+        """
+        return self.xml_builder.cancel_segment_rq(token_session,list_segment)
+    
     def search_flightrq(self,request_searh):
         """
         This function is for searching flight
---
+++ pygds/tests/sabre/displayPnr_test.py
@@ -0,0 +1,28 @@
+"""
+    This is for testing purposes like a suite.
+"""
+import os
+from pygds.amadeus.errors import ClientError, ServerError
+from pygds.core.security_utils import decode_base64
+from pygds.env_settings import get_setting
+from pygds import log_handler
+from pygds.sabre.client import SabreClient
+import time,sqlite3,json
+from os import getcwd,chdir,mkdir
+
+
+def test():
+    """ A suite of tests """
+
+    username = get_setting("SABRE_USERNAME")
+    pcc = get_setting("SABRE_PCC")
+    password = decode_base64(get_setting("SABRE_PASSWORD"))
+    url = "https://webservices3.sabre.com"
+ 
+    client = SabreClient(url, username, password, pcc, False)
+
+    display_pnr = client.get_reservation("LVWPSX", None)
+    return display_pnr
+
+if __name__ == "__main__":
+    print(test())
