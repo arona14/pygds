aa4e074792c886aac24d7db74c03fcc042fdd349
hannmbaye <mbaye@ctsfares.com>
2019-08-27 14:34:35 +0000

    build client send remark, extractor and test(sabre)

--- pygds/tests/sabre/test_all.py
+++ pygds/tests/sabre/test_all.py
@@ -23,45 +23,15 @@ def test():
 
     client = SabreClient(url, "", username, password, pcc, False)
 
-    segment_select = [1, 2, 3, 4]
-    passenger_type = [
-        {
-            "code": "JCB",
-            "nameSelect": [
-                "01.01"
-            ],
-            "quantity": 1
-        },
-        {
-            "code": "JCB",
-            "nameSelect": [
-                "02.01"
-            ],
-            "quantity": 1
-        },
-        {
-            "code": "J11",
-            "nameSelect": [
-                "03.01"
-            ],
-            "quantity": 1
-        },
-        {
-            "code": "JNF",
-            "nameSelect": [
-                "04.01"
-            ],
-            "quantity": 1
-        }
-    ]
-    display_pnr = client.get_reservation("XCVYRX", None)
+    display_pnr = client.get_reservation("TLRYVS", None)
     session_info = display_pnr.session_info
     if not session_info:
-        print("Awma session info")
+        print("No session info")
         return
     message_id = session_info.message_id
-    price = client.search_price_quote(message_id, retain=False, fare_type='Net', segment_select=segment_select, passenger_type=passenger_type)
-    print(price)
+    remark = client.send_remark(message_id, 'Virginie')
+    # price = client.search_price_quote(message_id, retain=False, fare_type='Net', segment_select=segment_select, passenger_type=passenger_type)
+    print(remark)
 
 
 if __name__ == "__main__":
