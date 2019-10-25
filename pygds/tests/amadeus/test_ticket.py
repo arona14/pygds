d28ee2bf23f84a5aa834393495b232c145b1d5bb
amadou <diallo1994>
2019-10-25 18:28:41 +0000

    add verification of dict  instance

--- pygds/tests/amadeus/test_ticket.py
+++ pygds/tests/amadeus/test_ticket.py
@@ -26,7 +26,7 @@ def test():
     os.makedirs(os.path.join(dir_path, "out"), exist_ok=True)
     log_handler.load_file_config(os.path.join(dir_path, "log_config.yml"))
     log = log_handler.get_logger("test_ticket")
-    pnr = "LNB4CC"  # "LNB4CC", "LN6C8E", "LMEBKP", "LC87DQ", "LBQ6P9", "L6LMQP", "KDN6HQ", "Q68EFX", "Q68EFX", "RI3B6D", "RT67BC", "RH3WOD", "WKHPRE", "TSYX56", "SNG6IR", "SY9LBS"
+    pnr = "LTGPDG"  # "LNB4CC", "LN6C8E", "LMEBKP", "LC87DQ", "LBQ6P9", "L6LMQP", "KDN6HQ", "Q68EFX", "Q68EFX", "RI3B6D", "RT67BC", "RH3WOD", "WKHPRE", "TSYX56", "SNG6IR", "SY9LBS"
     # m_id = None
 
     client = AmadeusClient(endpoint, username, password, office_id, wsap, True)
