7755bc28b9cf2ff56d5219d3944202915239b3cf
amadou <diallo1994>
2019-12-13 13:17:39 +0000

    test

--- pygds/tests/amadeus/test_low_fare_search_helper.py
+++ pygds/tests/amadeus/test_low_fare_search_helper.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from pygds.amadeus.xmlbuilders.low_fare_search_helper import generate_number_of_unit
-from pygds.core.types import TravellerNumbering, TravelFlightInfo
+from pygds.amadeus.xmlbuilders.low_fare_search_helper import generate_number_of_unit, generate_fare_options, generate_pax_reference
+from pygds.core.types import TravellerNumbering, TravelFlightInfo, FareOptions
 
 
 class LowFareSearchTest(TestCase):
@@ -23,6 +23,22 @@ class LowFareSearchTest(TestCase):
 
         self.assertEqual(content, result)
 
+    def test_fare_options(self):
+        fare = FareOptions()
+        result = generate_fare_options(fare)
+        self.assertIn("<priceType>RP</priceType>", result)
+        self.assertIn("<priceType>RU</priceType>", result)
+        self.assertIn("<priceType>ET</priceType>", result)
+        self.assertIn("<priceType>TAC</priceType>", result)
+        self.assertIn("<priceType>CUC</priceType>", result)
+        self.assertIn("<currency>EUR</currency>", result)
+
+    def test_generate_pax_reference(self):
+        traveler_number = TravellerNumbering(1, 1)
+        result = generate_pax_reference(traveler_number)
+        self.assertIn("<ptc>ADT</ptc><traveller><ref>1</ref></traveller>", result)
+        self.assertIn("<ptc>CH</ptc><traveller><ref>1</ref></traveller>", result)
+
 
 class TravelFlightInfoTest(TestCase):
 
@@ -32,10 +48,3 @@ class TravelFlightInfoTest(TestCase):
         self.assertEqual(flight_info.airlines, ["DL", "AF"])
         self.assertEqual(flight_info.rules_cabin, "RC")
         self.assertEqual(flight_info.rules_airline, "F")
-
-
-if __name__ == "__main__":
-    test = TravelFlightInfoTest()
-    test.test_travel_flight_info()
-    test = LowFareSearchTest()
-    test.test_generate_number_of_unit()
