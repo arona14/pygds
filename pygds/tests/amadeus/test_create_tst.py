1fe031917cb42b727ce1321c6ba36693d6817a7e
amadou <diallo1994>
2019-12-19 19:27:40 +0000

    add test

--- pygds/tests/amadeus/test_create_tst.py
+++ pygds/tests/amadeus/test_create_tst.py
@@ -13,27 +13,23 @@ class TestCreateTST(TestCase):
 
     def test_create_tst(self):
         result = self.tst_extractor._extract()
-        self.assertEqual(result[0].pnr, None)
-        self.assertEqual(result[0].status, None)
-        self.assertEqual(result[0].passengers[0].name_id, "2")
-        self.assertEqual(result[0].passengers[0].passenger_type, "PA")
+        self.assertEqual(result.pnr, None)
+        self.assertEqual(result.status, None)
+        self.assertEqual(result.passengers[0].name_id, "2")
+        self.assertEqual(result.passengers[0].passenger_type, "PA")
 
     def test_bulder_client(self):
-        tst_infos = [
-            TSTInfo(tst_ref="1", passengers=[PassengerBasicInfo(name_id="1", passenger_type="PA")]),
-            TSTInfo(tst_ref="2", passengers=[PassengerBasicInfo(name_id="2", passenger_type="PI")])
-        ]
+        tst_infos = TSTInfo(tst_ref="2", passengers=[PassengerBasicInfo(name_id="2", passenger_type="INF")])
         result = AmadeusXMLBuilder("", "", "", "", "").ticket_create_tst_from_price(None, None, None, None, tst_infos)
-        self.assertIn("<referenceDetails><type>PA</type><value>1</value></referenceDetails>", result)
         self.assertIn("<referenceDetails><type>PI</type><value>2</value></referenceDetails>", result)
 
     def tesy_class_tst_info(self):
-        tst_infos = TSTInfo(tst_ref="1", passengers=[PassengerBasicInfo(name_id="1", passenger_type="PA")])
+        tst_infos = TSTInfo(tst_ref="1", passengers=[PassengerBasicInfo(name_id="1", passenger_type="ADT")])
         self.assertEqual(tst_infos.pnr, None)
         self.assertEqual(tst_infos.status, None)
         self.assertEqual(tst_infos.tst_ref, "1")
         self.assertEqual(tst_infos.passengers[0].name_id, "1")
-        self.assertEqual(tst_infos.passengers[0].passenger_type, "PA")
+        self.assertEqual(tst_infos.passengers[0].passenger_type, "ADT")
 
 
 class TestDisplayTST(TestCase):
